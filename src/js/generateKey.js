121418cb4f35d64d0bed7f78d493a103d6222aab
Artemiy <artem2k17xia@yandex.ru>
2024-09-07 09:51:37 +0500

    новые игры

--- src/js/generateKey.js
+++ src/js/generateKey.js
@@ -1,128 +1,123 @@
 import { v4 as uuidv4 } from 'uuid';
 
-export const generateKey = (count) => {
- const APP_TOKEN = 'd28721be-fd2d-4b45-869e-9f253b554e50';
- const PROMO_ID = '43e35910-c168-4634-ad4f-52fd764a843f';
-
- const btn = document.querySelector('.btn');
- const btn2 = document.querySelector('.btn2');
- const wrapper = document.querySelector('.keys');
- const spinner = document.querySelector('.spinner');
- const img = document.querySelector('.img');
-
- const generateClientId = () => {
-  const timestamp = Date.now();
-  const randomNumbers = Array.from({ length: 19 }, () => Math.floor(Math.random() * 10)).join('');
-  return `${timestamp}-${randomNumbers}`;
- };
-
- const getClientToken = async () => {
-  spinner.textContent = 'получение токена клиента...';
-  const id = generateClientId();
-  const body = {
-   appToken: APP_TOKEN,
-   clientId: id,
-   clientOrigin: 'deviceid',
+export const generateKey = async ({ name, appToken, promoId }) => {
+ const APP_TOKEN = appToken;
+ const PROMO_ID = promoId;
+ let i = 1;
+ return await new Promise((res, rej) => {
+  const attemptCounter = (text) => {
+   if (i === 3) {
+    rej(text);
+   } else {
+    i++;
+    console.log(`Name: ${name}, попытка №${i}`);
+    generateRec();
+   }
   };
 
-  await fetch('https://api.gamepromo.io/promo/login-client', {
-   method: 'POST',
-   headers: {
-    'Content-Type': 'application/json',
-   },
-   body: JSON.stringify(body),
-  })
-   .then((res) => res.json())
-   .then(({ clientToken }) => {
-    console.log('clientToken', clientToken);
-    registerEvent(clientToken);
-   })
-   .catch((err) => {
-    btn.disabled = '';
-    btn2.disabled = '';
+  const generateRec = () => {
+   const generateClientId = () => {
+    const timestamp = Date.now();
+    const randomNumbers = Array.from({ length: 19 }, () => Math.floor(Math.random() * 10)).join('');
+    return `${timestamp}-${randomNumbers}`;
+   };
 
-    spinner.textContent = 'Ошибка! Не удалось создать токен клиента';
-    console.log(`не удалось создать clientToken. Ошибка: ${err}`);
-   });
- };
- const registerEvent = async (token) => {
-  spinner.textContent = 'регистрация события... Может занять до 3мин';
+   const getClientToken = async () => {
+    const id = generateClientId();
+    const body = {
+     appToken: APP_TOKEN,
+     clientId: id,
+     clientOrigin: 'deviceid',
+    };
 
-  const body = {
-   promoId: PROMO_ID,
-   eventId: uuidv4(),
-   eventOrigin: 'undefined',
-  };
-
-  console.log('EVENT', token);
-  const codeInterval = setInterval(async () => {
-   await fetch('https://api.gamepromo.io/promo/register-event', {
-    method: 'POST',
-    headers: {
-     'Content-Type': 'application/json',
-     Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(body),
-   })
-    .then((res) => res.json())
-    .then(({ hasCode }) => {
-     console.log(`hasCode: ${hasCode}`);
-     if (hasCode) {
-      generateKey(token);
-      clearInterval(codeInterval);
-     }
+    await fetch('https://api.gamepromo.io/promo/login-client', {
+     method: 'POST',
+     headers: {
+      'Content-Type': 'application/json',
+     },
+     body: JSON.stringify(body),
     })
-    .catch((err) => {
-     btn.disabled = '';
-     spinner.textContent = 'Ошибка! Не удалось зарегистрировать событие';
-     console.log(`не удалось зарегистрировать событие. Ошибка: ${err}`);
-    });
-  }, 25000);
- };
+     .then((res) => res.json())
+     .then(({ clientToken }) => {
+      console.log('clientToken', clientToken);
+      if (clientToken) {
+       registerEvent(clientToken);
+      } else {
+       attemptCounter('не удалось создать clientToken.');
+      }
+     })
+     .catch(() => {
+      attemptCounter('не удалось создать clientToken.');
+     });
+   };
+   const registerEvent = async (token) => {
+    const body = {
+     promoId: PROMO_ID,
+     eventId: uuidv4(),
+     eventOrigin: 'undefined',
+    };
 
- const generateKey = async (token) => {
-  spinner.textContent = 'генерация ключа...';
-  const body = {
-   promoId: PROMO_ID,
+    let count = 1;
+
+    const codeInterval = setInterval(async () => {
+     await fetch('https://api.gamepromo.io/promo/register-event', {
+      method: 'POST',
+      headers: {
+       'Content-Type': 'application/json',
+       Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(body),
+     })
+      .then((res) => res.json())
+      .then(({ hasCode }) => {
+       console.log(`hasCode: ${hasCode}`);
+       if (hasCode) {
+        generateKey(token);
+        clearInterval(codeInterval);
+       } else {
+        if (count === 7) {
+         clearInterval(codeInterval);
+         throw new Error();
+        }
+        count++;
+       }
+      })
+      .catch((err) => {
+       clearInterval(codeInterval);
+       attemptCounter(`не удалось зарегистрировать событие. Ошибка: ${err}`);
+      });
+    }, 25000);
+   };
+
+   const generateKey = async (token) => {
+    const body = {
+     promoId: PROMO_ID,
+    };
+
+    await fetch('https://api.gamepromo.io/promo/create-code', {
+     method: 'POST',
+     headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+     },
+     body: JSON.stringify(body),
+    })
+     .then((res) => res.json())
+     .then(({ promoCode }) => {
+      res({ name, promoCode });
+     })
+     .catch((err) => {
+      attemptCounter(`не удалось сгенерировать ключ. Ошибка: ${err}`);
+     });
+   };
+
+   const start = () => {
+    getClientToken();
+   };
+
+   start();
   };
 
-  await fetch('https://api.gamepromo.io/promo/create-code', {
-   method: 'POST',
-   headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${token}`,
-   },
-   body: JSON.stringify(body),
-  })
-   .then((res) => res.json())
-   .then(({ promoCode }) => {
-    if (count) {
-     setTimeout(() => start(), 2000);
-     spinner.textContent = `осталось: ${count}`;
-    } else {
-     spinner.textContent = 'готово!';
-     img.style.display = 'block';
-     btn.disabled = '';
-     btn2.disabled = '';
-    }
-    const elem = document.createElement('li');
-    elem.textContent = promoCode;
-    wrapper.append(elem);
-   })
-   .catch((err) => {
-    btn.disabled = '';
-    spinner.textContent = 'Ошибка! Не удалось сгенерировать ключ';
-    console.log(`не удалось сгенерировать ключ. Ошибка: ${err}`);
-   });
- };
-
- const start = () => {
-  if (count) count--;
-  img.style.display = 'none';
-  btn.disabled = 'true';
-  btn2.disabled = 'true';
-  getClientToken();
- };
-
- start();
+  generateRec();
+ });
 };
--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,101 @@ import '../style/global.scss';
 import { generateKey } from './generateKey';
 
 window.addEventListener('DOMContentLoaded', () => {
- const btn = document.querySelector('.btn');
+ //  const btn = document.querySelector('.btn');
+ const spinner = document.querySelector('.spinner');
  const btn2 = document.querySelector('.btn2');
+ const table = document.querySelector('.table');
+ const spinnerText = document.querySelector('.spinnerText');
+ const img = document.querySelector('.img');
 
- btn.addEventListener('click', generateKey);
+ const configs = [
+  {
+   name: 'Merge Away',
+   appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
+   promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
+  },
+  {
+   name: 'Chain Cube',
+   appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
+   promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
+  },
 
- btn2.addEventListener('click', () => {
-  generateKey(4);
+  {
+   name: 'Train Miner',
+   appToken: '82647f43-3f87-402d-88dd-09a90025313f',
+   promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
+  },
+  {
+   name: 'Twerk Race',
+   appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+   promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+  },
+  {
+   name: 'Polysphere',
+   appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+   promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+  },
+  {
+   name: 'Mow and Trim',
+   appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+   promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+  },
+  {
+   name: 'Mud Racing',
+   appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
+   promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
+  },
+  {
+   name: 'Zoopolis',
+   appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+   promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+  },
+ ];
+
+ configs.forEach((item) => {
+  const wrap = document.createElement('div');
+  const ol = document.createElement('ol');
+  const title = document.createElement('h2');
+  title.textContent = item.name;
+  ol.setAttribute('data-name', `${item.name}`);
+  wrap.append(title);
+  wrap.append(ol);
+  table.append(wrap);
+ });
+
+ //  btn.addEventListener('click', () => {
+ //   generateKey();
+ //  });
+
+ btn2.addEventListener('click', async () => {
+  let count = 8;
+  spinner.style.display = 'block';
+  spinnerText.textContent = `генерация ключей... осталось ${count} партий ключей`;
+  btn2.disabled = 'true';
+  img.style.display = 'none';
+
+  for (count; count > 0; count--) {
+   const promises = configs.map((elem) => generateKey(elem));
+   await Promise.allSettled(promises)
+    .then((res) => {
+     for (let { status, value } of res) {
+      if (status === 'fulfilled') {
+       const { name, promoCode } = value;
+       const elem = document.createElement('li');
+       elem.textContent = promoCode;
+       const wrapper = document.querySelector(`[data-name="${name}"`);
+       wrapper.append(elem);
+       if (!count) {
+        spinner.style.display = 'none';
+        spinnerText.textContent = `ГОТОВО!`;
+        img.style.display = 'block';
+       } else {
+        spinnerText.textContent = `генерация ключей... осталось ${count} партий ключей`;
+       }
+      }
+     }
+    })
+    .catch((err) => console.log(err));
+  }
  });
 });
